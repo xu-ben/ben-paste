f29031ee5287df94a6d304dfec6dc24071864305
Ben Xu <bjfuxuben@gmail.com>
2015-08-04 03:54:03 +0800

    增加README文件和dbconnect.php文件

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+﻿<?php
+	$dbhost = "localhost";
+	$dbuser = "zjwdb_322389";
+	$dbpw = "MySql123";
+	$dbname = "paste_tab";
+	$con = @mysql_connect($dbhost, $dbuser, $dbpw);
+	if(!$con) {
+		die ("无法连接数据库, 请核查后重试!");
+	}
+	mysql_select_db($dbname, $con);
+?>
